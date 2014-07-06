795b03a0f6bef62bc8f04930e4f279303edb52e9
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-07-06 16:41:10 +0200

    include helper

--- c++/test.cpp
+++ c++/test.cpp
@@ -3,16 +3,17 @@
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
-#include "pinput.h"
 #include "tools.h"
+#include "pinput.h"
 
 int main(int argc, char* argv[]){
 	std::vector<std::string> params;
 	std::unordered_map<std::string, std::vector<std::string>> options;
 	std::unordered_set<std::string> flags;
-	std::unordered_set<std::string> optional = {"--period", "--repair"};
+	std::unordered_set<std::string> option_set = {"--period", "--repair"};
+	std::unordered_set<std::string> flag_set = {"--help"};
 
-	pinput::parse(argc, argv, params, options, flags, optional);
+	pinput::parse(argc, argv, params, options, flags, option_set, flag_set);
 
 	std::cout << params << std::endl;
 	std::cout << options << std::endl;
